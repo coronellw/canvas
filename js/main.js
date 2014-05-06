8f63acbb5af2de1b1e077a27ee4e66da715b550b
Winston Coronell <coronell@gmail.com>
2014-05-06 15:16:24 -0500

    complete preview

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-var canvas, context, posx = 10, posy = 10, dirx = 1, diry = 1, allposx=[], allposy=[], moving = false;
+var canvas, context, posx = 10, posy = 10, dirx = 1, diry = 1, moving = false;
+var stage, sunShape, timer, countDown, count=0, interval_id = 0, timeTxt;
+
+window.onload = function(){
+	// moverMotor();
+}
 
 function moverMotor(){
 	var contenedor = document.getElementById("contenedor");
@@ -7,63 +12,101 @@ function moverMotor(){
 	canvas = document.createElement("canvas")
 	canvas.height = document.getElementById("alto").value;
 	canvas.width = document.getElementById("ancho").value;
-	/*dirx = document.getElementById("speed").value;
-	diry = document.getElementById("speed").value;*/
-	// alert("speed value = "+dirx);
+	dirx = parseInt(document.getElementById("speed").value);
+	diry = parseInt(document.getElementById("speed").value);
+	timer = parseInt(document.getElementById("time").value);
+	timer *=1000;
+	stage = new createjs.Stage(canvas);
 	context = canvas.getContext("2d");
 	contenedor.appendChild(canvas);
-	toggleAnimation();
-	animateMotor();
+
+	sunShape = new createjs.Shape();
+	sunShape.graphics.beginFill("#f00");
+	sunShape.graphics.drawCircle(0,0,10);
+	sunShape.graphics.endFill();
+	sunShape.x = 20;
+	sunShape.y = 20;
+	stage.addChild(sunShape);
+	count = 1;
+	moving = true;
+	countDown = timer-1000;
+	
+	console.log("timer begin... "+countDown);
+	if (timer > 0) {
+		interval_id = setInterval(function(){ 
+			countDown = timer - 1000*(count+1);
+			count++;
+			console.log(countDown/1000);
+		},1000);
+		window.setTimeout(function(){stopAnimation();},timer);
+	} 
+	
+	// createjs.Tween.get(sunShape, {loop:true}).to({x:240, y:240}, 1000).to({x:10,y:240}, 2000).to({x:240, y:10}, 2000).to({x:10, y:10},1000);
+
+	createjs.Ticker.addEventListener("tick", tickHandler);
 }
 
-function animateMotor(){
-	// alert("startAnimation");
-	if ((posx <= 5)||(posx >= canvas.width-5)) {
-		dirx*=-1;
-		// alert("cambio de dir en x "+dirx);
-	};
-
-	if ((posy <= 5)||(posy >= canvas.height-5)) {
-		diry*=-1;
-		// alert("cambio de dir en y "+diry);
-	};
-
-	posx = posx + dirx;
-	posy = posy + diry;	
-	allposy.push(posy);
-	allposx.push(posx);
-	// console.log(posx);
-
-	context.fillStyle = "black";
-	context.fillRect(0, 0, canvas.width, canvas.height);
-
-	context.fillStyle = "red";
-	context.beginPath();
-	context.arc(posx, posy, 10, 0, Math.PI*2);
-	context.fill();
+function tickHandler(e){
 	if (moving) {
-		setInterval(animateMotor(), 30);
-	};
+		moverPunto();
+		if (timer <= 0) {
+			timeTxt = "ciclico"
+		}else{
+			 timeTxt = countDown/1000 +" segundos";
+		}
+		document.getElementById("remaining").innerHTML = timeTxt;
+	}
+	stage.update();
 }
 
-function writeArray(arreglo){
-	for (var i = 0; i <= arreglo.length - 1; i++) {
-		console.log("Arreglo["+i+"]="+arreglo[i]);
+function moverPunto(){
+	if ((sunShape.x < 10)||(sunShape.x > canvas.width - 10)) {
+		dirx*=-1;
 	};
+	if ((sunShape.y < 10)||(sunShape.y > canvas.height - 10)) {
+		diry*=-1;
+	};
+
+	sunShape.x += dirx;
+	sunShape.y += diry;
 }
+
+function stopAnimation(){
+	createjs.Tween.get(sunShape, {loop:false}).to({x:10, y:10}, 1000);
+	moving = false;
+	clearInterval(interval_id);
+
+	toggleControls();
+	document.getElementById("control").innerHTML = "start";
+}
+
 
 function toggleAnimation(){
-	// alert(document.getElementById("control").innerHTML);
-	if(document.getElementById("control").innerHTML == "play"){
+	var texto = document.getElementById("control").innerHTML;
+	if( texto == "play"){
 		moving = true;
-		document.getElementById("control").innerHTMLalue = "pause";
+		document.getElementById("control").innerHTML = "pause";
 	}else{
-		if(document.getElementById("control").innerHTML == "pause"){
+		if (texto == "pause") {
 			moving = false;
 			document.getElementById("control").innerHTML = "play";
-		}else{
-			document.getElementById("control").innerHTML = "play";
+		} else{
+			console.log("Starting animation...");
+			moving = true;
+			toggleControls();			
 			moverMotor();
-		}
+			countDown = 0;
+			count = 0;
+			document.getElementById("remaining").innerHTML = timer/1000 +" segundos";
+			document.getElementById("control").innerHTML = "pause";
+		};
 	}
+}
+
+function toggleControls(){
+	document.getElementById("ancho").disabled = document.getElementById("ancho").disabled? false:true;
+	document.getElementById("alto").disabled = document.getElementById("alto").disabled? false:true;
+	document.getElementById("speed").disabled = document.getElementById("speed").disabled? false:true;
+	document.getElementById("time").disabled = document.getElementById("time").disabled? false:true;
+	document.getElementById("stop").disabled = document.getElementById("stop").disabled? false:true;
 }
